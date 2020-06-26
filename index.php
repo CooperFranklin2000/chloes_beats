500a64a01428dd95f20fe4acb923061fa16a89aa
CooperFranklin2000 <63026873+CooperFranklin2000@users.noreply.github.com>
2020-06-25 20:14:10 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+
+    
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+  />
+  <link
+    rel="stylesheet"
+    href="https://bootswatch.com/3/yeti/bootstrap.min.css"
+  />
+  <body>
+    <header class="text-center">
+      <h1>
+        Chloe's <span class="text-primary">Beats</span>
+        <i class="fas fa-music"></i>
+      </h1>
+    </header>
+    <main class="text-center" style="padding: 15px;">
+      <h1>Click Here to find Chloe's Sick Beats</h1>
+      <a
+        href="https://musiclab.chromeexperiments.com/Song-Maker/song/6198767440101376"
+        ><button class="btn btn-primary">Click me</button></a
+      >
+    </main>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </body>
+</html>
